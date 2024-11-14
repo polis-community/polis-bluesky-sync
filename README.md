ce98d971af1deae3033f1f44614e338dd3a2df9c
Jaseem Abid <me@jabid.in>
2024-11-14 18:30:39 +0000

    Sync to a starter pack and list

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Bluesky Starter Pack sync
 
-🔄 Sync a list of users in [accounts.txt] to a Bluesky [starter pack][sp].
+🔄 Sync a list of users in [accounts.txt] to a Bluesky [starter pack][sp] and [list].
 
-🐙 Add yourself to [accounts.txt] and send me a PR. Github actions will update the list after merge to main. 
-
+🐙 Add yourself to [accounts.txt] and send me a PR. Github actions will update the list after merge to main.
 
 ## Dev
 
-1. Create `.env` file with `AT_LOGIN`, `AT_PASSWORD` and `STARTER_PACK_URI`
+1. Create `.env` file with `AT_LOGIN`, `AT_PASSWORD`, `STARTER_PACK_URI` and `LIST_URI`
 2. `$ uv run --env-file .env sync.py`
 
 
@@ -15,3 +14,4 @@
 
 [sp]: https://bsky.app/starter-pack/jabid.in/3lagxhtghxi2e
 [accounts.txt]: https://github.com/jaseemabid/bluesky-sync/blob/main/accounts.txt
+[list]: https://bsky.app/profile/jabid.in/lists/3lawghh5a6v2c
--- sync.py
+++ sync.py
@@ -5,10 +5,13 @@ from datetime import timezone
 
 
 def main():
-    # Setup these 3 variables in a `.env` file or shell env
-    STARTER_PACK_URI = os.environ["STARTER_PACK_URI"]
+    # Setup these in an `.env` file, shell env or github actions.
     LOGIN = os.environ["AT_LOGIN"]
     PASSWORD = os.environ["AT_PASSWORD"]
+    LISTS = [
+        ("starter pack", os.environ["STARTER_PACK_URI"]),
+        ("list", os.environ["LIST_URI"]),
+    ]
 
     with open("accounts.txt") as f:
         expected = set(line.strip() for line in f.readlines() if line.strip())
@@ -18,52 +21,56 @@ def main():
 
     print(f"logged in as {profile.display_name}")
 
-    # List of members in the starter pack
-    members = client.app.bsky.graph.get_list({"list": STARTER_PACK_URI, "limit": 100})
+    for name, uri in LISTS:
+        # List of members in the starter pack/list
+        members = client.app.bsky.graph.get_list({"list": uri, "limit": 100})
 
-    print(f"Found {members.list.list_item_count} users, expected {len(expected)}")
-
-    # 🔥 TODO: Paginate this, get users after first 100
-    found = set(m.subject.handle for m in members.items)
-
-    # Step 1: Log extra users
-    extra = found - expected
-    if extra:
-        print(f"Found {len(extra)} unexpected users, moving on it: {(extra)}")
-
-    # Step 2: Add missing users
-    missing = expected - found
-    if not missing:
-        print("No new users to add, exiting")
-        return
-
-    print(f"Adding {len(missing)} users to the starter pack: {list(missing)}")
-
-    # ✨ Neat API to get all the missing users in one request
-    profiles = client.app.bsky.actor.get_profiles({"actors": list(missing)})
-    # ✨ All new users are added to the list in a single API
-    writes = [
-        models.ComAtprotoRepoApplyWrites.Create(
-            collection="app.bsky.graph.listitem",
-            value={
-                "$type": "app.bsky.graph.listitem",
-                "subject": p["did"],
-                "list": STARTER_PACK_URI,
-                "createdAt": datetime.now(timezone.utc).isoformat(),
-            },
+        print(
+            f"Found {members.list.list_item_count} users in {name}, expected {len(expected)}"
         )
-        for p in profiles.profiles
-    ]
 
-    data = models.ComAtprotoRepoApplyWrites.Data(repo="jabid.in", writes=writes)
+        # 🔥 TODO: Paginate this, get users after first 100
+        found = set(m.subject.handle for m in members.items)
 
-    print("Sending request")
-    print(data.model_dump())
+        # Step 1: Log extra users
+        unexpected = found - expected
+        if unexpected:
+            print(f"Ignoring {len(unexpected)} users not in text file: {(unexpected)}")
 
-    resp = client.com.atproto.repo.apply_writes(data)
+        # Step 2: Add missing users
+        missing = expected - found
+        if not missing:
+            print("No new users to add, exiting")
+            return
 
-    print("Got response")
-    print(resp)
+        if len(missing) > 25:
+            print(
+                "⚠️ Bluesky list API can only handle 25 items at a time",
+                "Update the code or split the data into smaller chunks",
+            )
+            exit(1)
+
+        print(f"Adding {len(missing)} users to the {name}: {missing}")
+
+        # ✨ Neat API to get all the missing users in one request
+        profiles = client.app.bsky.actor.get_profiles({"actors": list(missing)})
+
+        # ✨ All new users are added to the list in a single API
+        writes = [
+            models.ComAtprotoRepoApplyWrites.Create(
+                collection="app.bsky.graph.listitem",
+                value={
+                    "$type": "app.bsky.graph.listitem",
+                    "subject": p["did"],
+                    "list": uri,
+                    "createdAt": datetime.now(timezone.utc).isoformat(),
+                },
+            )
+            for p in profiles.profiles
+        ]
+
+        data = models.ComAtprotoRepoApplyWrites.Data(repo="jabid.in", writes=writes)
+        client.com.atproto.repo.apply_writes(data)
 
 
 if __name__ == "__main__":
